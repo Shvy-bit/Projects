d7cb0398a3681fd1f9f19d842beb7369d105b9fd
Shvy-bit <jcusiq@unsa.edu.pe>
2026-06-05 15:37:18 -0500

    index.html y generador de pdf

---
+++ Lab09/SisVentas/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import DetalleDeVentaPdf
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('venta/<int:sale_id>/pdf/', DetalleDeVentaPdf.as_view(), name='venta_pdf'),
+]
--- Lab09/SisVentas/views.py
+++ Lab09/SisVentas/views.py
@@ -1,3 +1,33 @@
+from django.views import View
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.shortcuts import get_object_or_404
+from xhtml2pdf import pisa
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Sale
+
+def index(request):
+    ventas = Sale.objects.all()
+    return render(request, 'index.html', {'VentasHtml': ventas})
+
+class DetalleDeVentaPdf(View):
+    def get(self, request, sale_id, *args, **kwargs):
+
+        venta = get_object_or_404(Sale, id=sale_id)
+        detalles = venta.sale_detail_set.all()
+        template = get_template('reportes/factura.html')
+        context = {
+            'venta': venta,
+            'detalles': detalles,
+        }
+
+        html_string = template.render(context)
+        
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = f"inline; filename='Venta {venta.id}.pdf'"
+
+        pisa_status = pisa.CreatePDF(html_string, dest=response)
+        if pisa_status.err:
+            return HttpResponse('Ocurrió un error al generar el PDF', status=500)
+        return response
